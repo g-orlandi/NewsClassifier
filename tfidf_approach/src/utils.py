d7bbcdcb322e9dabbf8560cfc3153de29e644083
Giovanni Orlandi <giovanni.orlandi@brainstorm.it>
2026-01-24 11:53:37 +0100

    t2a

--- tfidf_approach/src/utils.py
+++ tfidf_approach/src/utils.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from sklearn.metrics import ConfusionMatrixDisplay
 
 from .config import *
 from .preprocessing import initial_prep
+
 
 def load_data(path):
     df = pd.read_csv(path, index_col=0, na_values='\\N')
@@ -11,3 +13,10 @@ def load_data(path):
     df = initial_prep(df, dev=True)
 
     return df
+
+def plot_cm(cm, normalize=True, labels=range(0,7)):
+    if normalize:
+        cm = cm / cm.sum(axis=1, keepdims=True)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                              display_labels=labels)
+    disp.plot()
